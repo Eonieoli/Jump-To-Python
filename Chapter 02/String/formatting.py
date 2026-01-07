2af3fbf490e9d3057743c8560c71d3656d9adb02
Jaehyeok Kim <rublin322@gmail.com>
2026-01-07 16:50:14 +0900

    Chapter 02

--- Chapter 02/String/formatting.py
+++ Chapter 02/String/formatting.py
@@ -14,3 +14,12 @@ print("I eat %d apples." % number)
 number = 10
 day = "three"
 print("I ate %d apples. So I was sick for %s days." % (number, day))
+
+### %s 포맷코드는 어떤 형태의 값이든 변환해서 넣을 수 있다.
+print("I have %s apples." % 3)
+print("rate is %s" % 3.234)
+### 포맷코드를 쓰는 동시에 '%'문자 자체도 쓰고 싶으면 문자열 안에서 %%를 써야 한다.
+# print("Error is %d%." % 98)
+print("Error is %d%%." % 98)
+
+# 포맷 코드와 숫자 함께 사용하기
