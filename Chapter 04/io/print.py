4de93ae46dcaa774660baabaa8b6ef5db9622cf0
김재혁 <rublin322@gmail.com>
2026-01-08 22:27:42 +0900

    Chapter 04

---
+++ Chapter 04/io/print.py
@@ -0,0 +1,14 @@
+a = 123
+print(a)
+a = "Python"
+print(a)
+a = [1, 2, 3]
+print(a)
+
+print("life" "is" "too short")
+print("life"+"is"+"too short")
+
+print("life", "is", "too short")
+
+for i in range(10):
+    print(i, end=' ')
