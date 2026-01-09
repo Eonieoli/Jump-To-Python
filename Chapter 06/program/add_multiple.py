4671d89f53d05ff63702b073e87e8f1f4c7b8c60
Jaehyeok Kim <rublin322@gmail.com>
2026-01-09 16:55:15 +0900

    Chapter 06

--- Chapter 06/program/add_multiple.py
+++ Chapter 06/program/add_multiple.py
@@ -1,7 +1,7 @@
 def add_multiple(num1, num2):
     total = 0
-    for i in range(1000):
-        if i % num1 == 0 | i % num2 == 0:
+    for i in range(1, 1000):
+        if i % num1 == 0 or i % num2 == 0:
             total += i
     return total
 
