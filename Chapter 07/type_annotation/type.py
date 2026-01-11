4a6b20b54f4041714a73ea004c1833eb98ba6e00
김재혁 <rublin322@gmail.com>
2026-01-11 23:22:14 +0900

    Chapter 07

---
+++ Chapter 07/type_annotation/type.py
@@ -0,0 +1,5 @@
+a = 1
+print(type(a))
+
+a = "1"
+print(type(a))
---
+++ Chapter 07/type_annotation/typing_sample.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+# result = add(3, 3.4)
+# print(result)
+
+result = add(3, 4)
+print(result)
