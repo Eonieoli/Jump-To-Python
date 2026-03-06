b7dd90f3ca29748f1fad1c842dde486ba606fa9c
Jaehyeok Kim <rublin322@gmail.com>
2026-03-06 16:28:15 +0900

    Problem

--- Problem/problem.py
+++ Problem/problem.py
@@ -256,22 +256,9 @@
 
 
 # 18
-import re
+# import re
 
-p = re.compile("[a-z]+")
-m = p.search("5 python")
-print(m.start() + m.end())
-# 2 + 8 = 10
-
-
-# 19
-import re
-
-text = """
-park 010-9999-9988
-kim 010-9909-7789
-lee 010-8789-7768
-"""
-
-pattern = re.compile(r"\w+\s\d+[-]\d+[-](\d+)")
-pattern.sub("\g<1>")
+# p = re.compile("[a-z]+")
+# m = p.search("5 python")
+# print(m.start() + m.end())
+# # 2 + 8 = 10
